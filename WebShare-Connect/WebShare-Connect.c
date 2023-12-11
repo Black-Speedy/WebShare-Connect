a212c5204f26c8134f34f4e8fa6b59c656ca97b1
Black Speedy <78355795+Black-Speedy@users.noreply.github.com>
2023-12-11 21:37:57 +0100

    Start I think?

---
+++ WebShare-Connect/WebShare-Connect.c
@@ -0,0 +1,8 @@
+﻿// WebShare-Connect.cpp : Defines the entry point for the application.
+//
+
+#include "WebShare-Connect.h"
+
+int main(){
+	printf("Hello CMake.\n");
+}
---
+++ WebShare-Connect/WebShare-Connect.h
@@ -0,0 +1,8 @@
+﻿// WebShare-Connect.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <stdio.h>
+
+// TODO: Reference additional headers your program requires here.
