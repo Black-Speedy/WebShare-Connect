e5b07a23a81dc973088ac448bb1388176dde0b73
Black-Speedy <78355795+Black-Speedy@users.noreply.github.com>
2024-01-15 01:04:31 +0100

    Some code cleanup plus added back that you can write server and client, but kept it so you still can write sender and receiver, they are both valid now

--- WebShare-Connect/WebShare-Connect.c
+++ WebShare-Connect/WebShare-Connect.c
@@ -2,12 +2,12 @@
  * @file WebShare-Connect.c
  * @brief Main program file containing the entry point.
  */
-
+#include <stdio.h>
 #include "WebShare-Connect.h"
 #include "sender.h"
 #include "receiver.h"
 #include "removeQuotes.h"
-#include "nice.h"
+#include <nice.h>
 
  /**
   * @brief The main function. This is where the program starts.
@@ -24,25 +24,36 @@
   */
 int main(int argc, char const* argv[])
 {
+    printf("How many arguments: %d\n", argc);
+    char* user_argv[5]; // Array of arguments to pass to sender_main or receiver_main
+    if (5 == argc) {
+        printf("IM HERE\n");
+        // set user_argv to argv
+        for (int i = 0; i < argc; i++) {
+            user_argv[i] = strdup(argv[i]);
+        }
+    }
     //if there are no arguments then ask for them
-    if (argc < 2) {
+    // else if (argc < 2) {
+    else {
         char mode[10];
         int error;
         char port[6];
-		char threads[4];
+        char threads[4];
         char filePath[256];
-        char* user_argv[5]; // Array of arguments to pass to sender_main or receiver_main
         do {
-            printf("Please enter either 'sender' or 'receiver': ");
+            printf("Please enter either 'sender / server' or 'receiver / client': ");
             error = scanf("%9s", mode);
             if (error != 1) {
                 printf("Input error. Please try again.\n");
                 error = scanf("%*[^\n]"); // Clear the input buffer
             }
-            else if (strcmp(mode, "sender") != 0 && strcmp(mode, "receiver") != 0) {
+            else if (strcmp(mode, "sender") != 0 && strcmp(mode, "receiver") != 0 
+                    && strcmp(mode, "server") != 0 && strcmp(mode, "client") != 0) {
                 printf("Invalid mode entered. Please try again.\n");
             }
-        } while (strcmp(mode, "sender") != 0 && strcmp(mode, "receiver") != 0);
+        } while (strcmp(mode, "sender") != 0 && strcmp(mode, "receiver") != 0 
+                && strcmp(mode, "server") != 0 && strcmp(mode, "client") != 0);
 
         do {
             printf("Please enter the port (0-65535): ");
@@ -68,12 +79,12 @@ int main(int argc, char const* argv[])
                 error = scanf("%*[^\n]"); // Clear the input buffer
             }
             //else {
-                int threadNum = atoi(threads);
-                //if (threadNum < 1 || threadNum > MAX_THREADS) {
-                //    printf("Invalid number of threads. Please enter a number between 1 and %d.\n", MAX_THREADS);
-                //    error = 0;
-                //}
+            int threadNum = atoi(threads);
+            //if (threadNum < 1 || threadNum > MAX_THREADS) {
+            //    printf("Invalid number of threads. Please enter a number between 1 and %d.\n", MAX_THREADS);
+            //    error = 0;
             //}
+        //}
         } while (error != 1);
 
         int c;
@@ -94,22 +105,14 @@ int main(int argc, char const* argv[])
         }
         else user_argv[4] = strdup(filePath);
 
-        if (strcmp(mode, "sender") == 0) {
-            return sender_main(4, user_argv + 1); // 4 is the number of arguments in user_argv
-        }
-        else {
-            return receiver_main(4, user_argv + 1);
-        }
+    if (strcmp(user_argv[1], "sender") == 0 || strcmp(user_argv[1], "server") == 0) {
+        return sender_main(argc - 1, user_argv + 1);
     }
-
-    if (strcmp(argv[1], "sender") == 0) {
-        return sender_main(argc - 1, argv + 1); //what does argv + 1 do? a: it is a pointer to the next element in the array of arguments
-    }
-    else if (strcmp(argv[1], "receiver") == 0) {
-        return receiver_main(argc - 1, argv + 1);
+    else if (strcmp(user_argv[1], "receiver") == 0 || strcmp(user_argv[1], "client") == 0) {
+        return receiver_main(argc - 1, user_argv + 1);
     }
     else {
-        printf("Usage: %s [sender|receiver]\n", argv[0]);
-		return 1;
+        printf("Usage: %s [sender|receiver]\n", user_argv[0]);
+        return 1;
     }
 }
