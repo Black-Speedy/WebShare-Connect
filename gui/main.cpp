47f26ee192b3a8f2567d6f69e24d0d5adbe2a50c
unknown <mikail.oruc@live.dk>
2025-07-08 02:24:51 +0200

    boilerplate config and a bit of ui shcnizz

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,10 +1,21 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+
+    QFile f(":/themes/default.qss");
+    if (f.open(QFile::ReadOnly)) {
+        QString style = QLatin1String(f.readAll());
+        a.setStyleSheet(style);
+    } else {
+        qDebug() << "Failed to open stylesheet:" << f.errorString();
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
