4791f330371b6b142cb88dab3946b88c0bd2e57b
Black Speedy <78355795+Black-Speedy@users.noreply.github.com>
2025-07-30 15:49:40 +0200

    Small update to tests (no longer need to in test files to include full relative path)

--- tests/test_cli_options.c
+++ tests/test_cli_options.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
 
-#include "../wsc/options.h"
-#include "../core/errorcodes.h"
+#include "options.h"
+#include "errorcodes.h"
 
 int call_help_handler(const char *arg[]) {
     return help_handler(NULL);
@@ -11,9 +11,11 @@ int call_help_handler(const char *arg[]) {
 int call_version_handler(const char *arg[]) {
     return version_handler(NULL);
 }
+
 int call_port_handler(const char *args[]) {
     return port_handler(args);
 }
+
 // int call_ip_handler(const char *args[]) {
 //     return ip_handler(args);
 // }
@@ -26,44 +28,44 @@ int main() {
 
     // Test help handler
     const char *help_args[] = {NULL};
-    int help_result = call_help_handler(help_args);
+    int        help_result  = call_help_handler(help_args);
     assert(help_result == 0);
     printf("Help handler test PASSED\n");
 
     // Test version handler
     const char *version_args[] = {NULL};
-    int version_result = call_version_handler(version_args);
+    int        version_result  = call_version_handler(version_args);
     assert(version_result == 0);
     printf("Version handler test PASSED\n");
 
     // Test port handler with valid argument
     const char *port_args[] = {"8080"};
-    int port_result = call_port_handler(port_args);
+    int        port_result  = call_port_handler(port_args);
     assert(port_result == 0);
     printf("Correct port test PASSED\n");
 
     const char *port_args1[] = {"0"};
-    int port_result1 = call_port_handler(port_args1);
+    int        port_result1  = call_port_handler(port_args1);
     assert(port_result1 == ERR_INVALID_PORT);
     printf("Too small port test PASSED\n");
 
     const char *port_args2[] = {"65536"};
-    int port_result2 = call_port_handler(port_args2);
+    int        port_result2  = call_port_handler(port_args2);
     assert(port_result2 == ERR_INVALID_PORT);
     printf("Too large port test PASSED\n");
 
     const char *port_args3[] = {"-1"};
-    int port_result3 = call_port_handler(port_args3);
+    int        port_result3  = call_port_handler(port_args3);
     assert(port_result3 == ERR_INVALID_PORT);
     printf("Negative port test PASSED\n");
 
     const char *port_args4[] = {NULL};
-    int port_result4 = call_port_handler(port_args4);
+    int        port_result4  = call_port_handler(port_args4);
     assert(port_result4 == ERR_PORT_MISSING_VALUE);
     printf("No port test PASSED\n");
 
     const char *port_args5[] = {"eighty"};
-    int port_result5 = call_port_handler(port_args5);
+    int        port_result5  = call_port_handler(port_args5);
     assert(port_result5 == ERR_INVALID_PORT);
     printf("Invalid port test PASSED\n");
 
